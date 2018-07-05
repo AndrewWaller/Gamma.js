7ba3e9726294ad6959fc3b05e15a7ea2676dda99
AndrewWaller <awalldrew2@gmail.com>
2018-07-05 14:06:39 -0700

    trying to get app.js functions to run

--- Gamma.js
+++ Gamma.js
@@ -4,7 +4,8 @@
         return new Gamma.init(firstName, lastName, language);
     }
 
-    let supportedLangs = ['en', 'es'];
+    // Every Unicode Supported Language
+    let supportedLangs = ['ab', 'aa', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az', 'bm', 'ba', 'eu', 'be', 'bn', 'bh', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'ch', 'ce', 'ny', 'zh', 'zh-Hans', 'zh-Hant', 'cv', 'kw', 'co', 'cr', 'hr', 'cs', 'da', 'dv', 'nl', 'dz', 'en', 'eo', 'et', 'ee', 'fo', 'fj', 'fi', 'fr', 'ff', 'gl', 'gd', 'gv', 'ka', 'de', 'el', 'kl', 'gn', 'gu', 'ht', 'ha', 'he', 'hz', 'hi', 'ho', 'hu', 'is', 'io', 'ig', 'id', 'in', 'ia', 'ie', 'iu', 'ik', 'ga', 'it', 'ja', 'jv', 'kl', 'kn', 'kr', 'ks', 'kk', 'km', 'ki', 'rw', 'rn', 'ky', 'kv', 'kg', 'ko', 'ku', 'kj', 'lo', 'la', 'lv', 'li', 'ln', 'lt', 'lu', 'lg', 'lb', 'gv', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mh', 'mo', 'mn', 'na', 'nv', 'ng', 'nd', 'ne', 'no', 'nb', 'nn', 'ii', 'oc', 'oj', 'cu', 'or', 'om', 'os', 'pi', 'ps', 'fa', 'pl', 'pt', 'pa', 'qu', 'rm', 'ro', 'ru', 'se', 'sm', 'sg', 'sa', 'sr', 'sh', 'st', 'tn', 'sn', 'ii', 'sd', 'si', 'ss', 'sk', 'sl', 'so', 'nr', 'es', 'su', 'sw', 'ss', 'sv', 'tl', 'ty', 'tg', 'ta', 'tt', 'te', 'th', 'bo', 'ti', 'to', 'ts', 'tr', 'tk', 'tw', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'cy', 'wo', 'fy', 'xh', 'ji', 'yo', 'za', 'zu'];
 
     let greetings = {
         en: 'Hello',
@@ -60,28 +61,52 @@
             if (console) {
                 console.log(logMessages[this.language] + ': ' + this.fullName());
             }
-
             return this;
-
         },
 
         setLang: function(lang) {
             this.language = lang;
             this.validate();
             return this;
+        },
+
+         getAbsoluteUrl: function() {
+            var a;
+            return function(url) {
+                if(!a) a = document.createElement('a');
+                a.href = url;
+                return a.href;
+            };
+        },
+
+        sheet: function() {
+            // Create the <style> tag
+            var style = document.createElement('style');
+            // WebKit hack :(
+            style.appendChild(document.createTextNode(''));
+            // Add the <style> element to the page
+            document.head.appendChild(style);
+            return style.sheet;
+        },
+
+        toCelsius(f) {
+            return (5/9) * (f-32);
         }
 
     };
 
-    Gamma.init = function(firstName, lastName, language) {
+    Gamma.init = function(firstName, lastName, language, age, gender, weight) {
         var self = this;
         self.firstName = firstName || "";
         self.lastName = lastName || "";
         self.language = language || "en";
+        self.age = age || "";
+        self.gender = gender || "";
+        self.weight = weight || "";
     }
 
     Gamma.init.prototype = Gamma.prototype;
 
-    global.Gamma = global.Rx = global.rx = global.shiz = Gamma;
+    global.Gamma = global.Rx = global.rx = Gamma;
 
 }(window, $));
--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var g = rx("john");
-let ababa = shiz("dafuq");
+var g = rx("john", "daniel", "lewis");
+let ababa = rx("dafuq");
 console.log(g, ababa);
 
 g.greet().setLang('es').greet(false);
 
 g.greet().setLang('en').greet(true);
+
+g.sheet().insertRule("header { float: left; opacity: 0.8; }");
+
+var x = document.getElementById('fahrenheit').value;
+submitTemp() {
+    document.getElementById('tempResult').innerHTML =  x.toCelsius(x)
+    console.log(x);
+};
+
+FigureBMI(form,meters,kilograms) {
+    Square=meters*meters 
+    form.calcval.value=kilograms/Square
+};
+if(window!=window.top)top.location.href=location.href;
